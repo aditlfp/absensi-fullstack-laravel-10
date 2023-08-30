3e7fece0a3b8b6890648dd4b6277f117d6abdb69
aditlfp <aditya.budilfp11@gmail.com>
2023-08-30 08:26:31 +0700

    Update Schedule Point Auto Claim

---
+++ app/Console/Commands/PointUpdate.php
@@ -0,0 +1,46 @@
+<?php
+
+namespace App\Console\Commands;
+
+use App\Models\Absensi;
+use App\Models\Point;
+use App\Models\User;
+use Illuminate\Console\Command;
+
+class PointUpdate extends Command
+{
+    /**
+     * The name and signature of the console command.
+     *
+     * @var string
+     */
+    protected $signature = 'app:point-update';
+
+    /**
+     * The console command description.
+     *
+     * @var string
+     */
+    protected $description = 'Command description';
+
+    /**
+     * Execute the console command.
+     */
+    public function handle()
+    {
+        $abs = Absensi::all();
+        $point = Point::all();
+        
+    
+        foreach ($abs as $key) {
+            if ($key->keterangan == 'masuk') {
+                foreach ($point as $value) {
+                    if ($key->point_id == $value->id && $key->kerjasama->client_id == $value->client_id && $key->point_id == null) {
+                        $cId = $key->kerjasama->where('client_id', $value->client_id);
+                        Absensi::where('id', $key->id)->update(['point_id', $cId->point_id]);
+                    }
+                }
+            }
+        }
+    }
+}
