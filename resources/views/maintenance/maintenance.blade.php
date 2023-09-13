9a1c50220d2ad7492e4e1230087ecfd01f08567d
aditlfp <aditya.budilfp11@gmail.com>
2023-09-13 14:18:33 +0700

    update rating

---
+++ resources/views/maintenance/maintenance.blade.php
@@ -0,0 +1,10 @@
+<x-app-layout>
+    <x-main-div>
+        <img src="https://www.svgrepo.com/show/426192/cogs-settings.svg" alt="Logo" class="mb-8 h-40">
+        <h1 class="text-4xl md:text-5xl lg:text-6xl font-bold text-center text-gray-700 mb-4">Site is under maintenance</h1>
+        <p class="text-center text-gray-500 text-lg md:text-xl lg:text-2xl mb-8">We're working hard to improve the user experience. Stay tuned!</p>
+        <div class="flex space-x-4">
+          <a href="#" class="border-2 border-gray-800 text-black font-bold py-3 px-6 rounded">Back</a>
+        </div>
+    </x-main-div>
+</x-app-layout>
